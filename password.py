da771c37b7e29618dbef54d4130733162ff73517
Ruundii <alexey.outkine@gmail.com>
2020-08-06 18:32:52 +0100

    password change fix

--- password.py
+++ password.py
@@ -3,11 +3,11 @@
 import crypt,  spwd, subprocess
 
 def is_valid_current_password(current_password):
-    password = spwd.getspnam('ruundii').sp_pwdp
+    password = spwd.getspnam('pi').sp_pwdp
     return crypt.crypt(current_password, password)==password
 
 def set_new_password(new_password):
     p = subprocess.Popen([ "/usr/sbin/chpasswd" ], universal_newlines=True, shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (stdout, stderr) = p.communicate('ruundii' + ":" + new_password + "\n")
+    (stdout, stderr) = p.communicate('pi' + ":" + new_password + "\n")
     assert p.wait() == 0
     return not (stdout or stderr)

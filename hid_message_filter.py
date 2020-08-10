4b7a306a2dc29fb2410092e262038ea5798a00f4
Ruundii <alexey.outkine@gmail.com>
2020-08-10 20:10:35 +0100

    fix for default hid filter

--- hid_message_filter.py
+++ hid_message_filter.py
@@ -3,11 +3,11 @@
 class HIDMessageFilter:
 
     def filter_message_to_host(self, msg):
-        if len(msg) != 7:
+        if len(msg) < 7:
             return None
-        msg = b'\xa1' + msg
+        msg = b'\xa1\x01' + msg
         return msg
 
 
     def filter_message_from_host(self, msg):
-        return None
+        return msg[1:]

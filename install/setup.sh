fd027910555cdcd53f71c234d68ab51235ec1c8d
Sam Bull <git@sambull.org>
2023-09-10 19:29:06 +0100

    Updates for bullseye release (#61)

--- install/setup.sh
+++ install/setup.sh
@@ -1,11 +1,9 @@
-echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null
-
 cd /home/pi
 
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
 git clone https://github.com/ruundii/bthidhub
 cd /home/pi/bthidhub/install/on_rpi
-sh ./on_pi_setup.sh
+bash ./on_pi_setup.sh

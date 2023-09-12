32d1d214703f125e141247d2bf353033d4e11fe8
Sam Bull <git@sambull.org>
2023-09-12 19:30:57 +0100

    Fix HID devices on bluez (#64)

--- install/on_rpi/config_replacer.py
+++ install/on_rpi/config_replacer.py
@@ -7,7 +7,7 @@ tmp = tempfile.mkstemp()
 with open('/lib/systemd/system/bluetooth.service') as fd1, open(tmp[1], 'w') as fd2:
     for line in fd1:
         if line.startswith("ExecStart"):
-            line = "ExecStart=/usr/libexec/bluetooth/bluetoothd -p time,input,autopair,policy,scanparam,deviceinfo"
+            line = "ExecStart=/usr/libexec/bluetooth/bluetoothd -p time,input,hog,autopair,policy,scanparam,deviceinfo"
         fd2.write(line)
 
 os.rename(tmp[1], '/lib/systemd/system/bluetooth.service')

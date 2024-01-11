ruundii/bthidhub

commit 9050f67cc4e3265efed6b9f9390342c185ed0d61

replace all '/home/pi' with $HOME (#79)

install/on_rpi/on_pi_setup.sh
@@ -1,4 +1,4 @@
-cd /home/pi/bthidhub/install/on_rpi
+cd $HOME/bthidhub/install/on_rpi
 
 sudo echo 0 | sudo tee /sys/class/leds/ACT/brightness > /dev/null
 
@@ -21,11 +21,11 @@ systemctl --user disable obex
 systemctl --user mask obex
 
 sudo apt-get install libcairo2-dev libdbus-1-dev libgirepository1.0-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev autoconf automake libtool python3-pip -y
-sudo pip3 install -r /home/pi/bthidhub/requirements.txt
+sudo pip3 install -r $HOME/bthidhub/requirements.txt
 
-cd /home/pi/bthidhub/install/on_rpi
+cd $HOME/bthidhub/install/on_rpi
 git clone https://github.com/Dreamsorcerer/bluez.git
-cd /home/pi/bthidhub/install/on_rpi/bluez
+cd $HOME/bthidhub/install/on_rpi/bluez
 autoreconf -fvi
 
 ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --disable-a2dp --disable-avrcp --disable-network
@@ -35,12 +35,12 @@ make -j4
 sudo systemctl disable bluetooth
 sudo systemctl stop bluetooth
 sudo make install
-sudo python3 /home/pi/bthidhub/install/on_rpi/config_replacer.py
-sudo cp /home/pi/bthidhub/install/on_rpi/sdp_record.xml /etc/bluetooth/sdp_record.xml
-sudo cp /home/pi/bthidhub/install/on_rpi/input.conf /etc/bluetooth/input.conf
-sudo cp /home/pi/bthidhub/install/on_rpi/main.conf /etc/bluetooth/main.conf
+sudo python3 $HOME/bthidhub/install/on_rpi/config_replacer.py
+sudo cp $HOME/bthidhub/install/on_rpi/sdp_record.xml /etc/bluetooth/sdp_record.xml
+sudo cp $HOME/bthidhub/install/on_rpi/input.conf /etc/bluetooth/input.conf
+sudo cp $HOMR/bthidhub/install/on_rpi/main.conf /etc/bluetooth/main.conf
 
-sudo cp /home/pi/bthidhub/install/on_rpi/remapper.service /lib/systemd/system/remapper.service
+sudo cp $HOME/bthidhub/install/on_rpi/remapper.service /lib/systemd/system/remapper.service
 sudo chmod 644 /lib/systemd/system/remapper.service
 sudo systemctl daemon-reload
 
@@ -54,7 +54,7 @@ sudo sed -Ei 's/^127\.0\.1\.1.*$/127.0.1.1\tbthidhub/' /etc/hosts
 
 # Compile some Python modules to reduce lag.
 # We do this at the end, as the project is already usable without this step.
-cd /home/pi/bthidhub/
+cd $HOME/bthidhub/
 mypyc
 
 sudo reboot

install/setup.sh
@@ -1,9 +1,9 @@
-cd /home/pi
+cd $HOME
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install git -y
 
 git clone https://github.com/ruundii/bthidhub
-cd /home/pi/bthidhub/install/on_rpi
+cd $HOME/bthidhub/install/on_rpi
 bash ./on_pi_setup.sh

commit cd6773b84a80d0ec22ce0e09385b18c679bac7cc

correct typo of $HOMR to $HOME introduced in 9050f67

install/on_rpi/on_pi_setup.sh
@@ -38,7 +38,7 @@ sudo make install
 sudo python3 $HOME/bthidhub/install/on_rpi/config_replacer.py
 sudo cp $HOME/bthidhub/install/on_rpi/sdp_record.xml /etc/bluetooth/sdp_record.xml
 sudo cp $HOME/bthidhub/install/on_rpi/input.conf /etc/bluetooth/input.conf
-sudo cp $HOMR/bthidhub/install/on_rpi/main.conf /etc/bluetooth/main.conf
+sudo cp $HOME/bthidhub/install/on_rpi/main.conf /etc/bluetooth/main.conf
 
 sudo cp $HOME/bthidhub/install/on_rpi/remapper.service /lib/systemd/system/remapper.service
 sudo chmod 644 /lib/systemd/system/remapper.service
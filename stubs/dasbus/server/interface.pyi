e490a33be753ebe29ee68e389d961b27cf0e3159
Sam Bull <git@sambull.org>
2025-01-06 19:51:11 +0000

    Migrate project to a new home

--- stubs/dasbus/server/interface.pyi
+++
@@ -1,5 +0,0 @@
-from typing import Callable, TypeVar
-
-_T = TypeVar("_T", bound=type[object])
-
-def dbus_interface(interface_name: str, namespace: tuple[str, ...] = ...) -> Callable[[_T], _T]: ...

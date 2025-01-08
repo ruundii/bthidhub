6faf86fd995e633ef4414162eb5acce50371e8a2
Sam Bull <git@sambull.org>
2025-01-08 18:49:44 +0000

    Migrate project to a new home (#113)

--- install/install_windows.bat
+++
@@ -1,1 +0,0 @@
-plink -ssh -v -pw raspberry pi@192.168.0.31 -m setup.sh
--- install/setup.sh
+++
@@ -1,9 +0,0 @@
-cd $HOME
-
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install git -y
-
-git clone https://github.com/ruundii/bthidhub
-cd $HOME/bthidhub/install/on_rpi
-bash ./on_pi_setup.sh
